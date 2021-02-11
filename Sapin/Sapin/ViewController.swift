a084d1f08eb9a5e63f3b303c0ecf305acbf9245a
Jaakkx <78911141+Jaakkx@users.noreply.github.com>
2021-02-11 14:57:34 +0100

    initial commit

---
+++ Sapin/Sapin/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Sapin
+//
+//  Created by Jael Beining on 11/02/2021.
+//  Copyright © 2021 Jael Beining. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var premierTest: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
